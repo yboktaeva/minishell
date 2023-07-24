b444a1a05a5b08729fae1658919fb6fc61919733
Yulius <46687114+yboktaeva@users.noreply.github.com>
2023-07-24 19:14:56 +0200

    Delete obj directory

--- obj/main.d
+++
@@ -1,8 +0,0 @@
-obj/main.o: src/main.c src/../includes/minishell.h \
-  includes/../../libft/libft.h src/../includes/struct.h
-
-src/../includes/minishell.h:
-
-includes/../../libft/libft.h:
-
-src/../includes/struct.h:
--- obj/path.d
+++
@@ -1,8 +0,0 @@
-obj/path.o: src/path.c src/../includes/minishell.h \
-  includes/../../libft/libft.h src/../includes/struct.h
-
-src/../includes/minishell.h:
-
-includes/../../libft/libft.h:
-
-src/../includes/struct.h:
